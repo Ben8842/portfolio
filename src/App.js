b99bfb61448510e6bab903ad525eaf9c896424d7
Ben8842 <z87e99b44r55a@gmail.com>
2021-02-02 16:25:38 -0500

    adding project links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      Portfolio.
+      <div>
+        <div>
+          <a href="https://trivia-redux.herokuapp.com/">TRIVIA</a>
+        </div>
+        <div>
+          <a href="https://emojii-matching.herokuapp.com/">EMOJII MATCHING</a>
+        </div>
+        <div>
+          <a href="https://wordsearch-generator.herokuapp.com/">
+            WORD SEARCH GENERATOR
+          </a>
+        </div>
+        <a href="https://benwperkins.github.io/BitProject/">
+          Cryptocurrency Project
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
