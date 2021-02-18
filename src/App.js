a8d73f6e4d2ff2d6f90cf1cb370cbafb858d669b
Ben8842 <z87e99b44r55a@gmail.com>
2021-02-17 23:05:38 -0500

    css work

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import "./App.css";
-import Button from "react-bootstrap/Button";
+//import Button from "react-bootstrap/Button";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="title">
       Portfolio.
       <div>
-        <div>
-          <Button variant="primary" size="lg" block>
+        <div class="fancy">
+          <button class="fancy" variant="primary" size="lg" block>
             <a href="https://trivia-redux.herokuapp.com/">TRIVIA</a>
-          </Button>
+          </button>
         </div>
-        <Button variant="secondary" size="lg" block>
+        <button class="fancy" variant="secondary" size="lg" block>
           <a href="https://emojii-matching.herokuapp.com/">EMOJII MATCHING</a>
-        </Button>
-        <Button variant="primary" size="lg" block>
+        </button>
+        <button class="fancy" variant="primary" size="lg" block>
           <a href="https://wordsearch-generator.herokuapp.com/">
             WORD SEARCH GENERATOR
           </a>
-        </Button>
-        <Button variant="secondary" size="lg" block>
+        </button>
+        <button class="fancy" variant="secondary" size="lg" block>
           <a href="https://benwperkins.github.io/BitProject/">
             Cryptocurrency Project
           </a>
-        </Button>
+        </button>
         <div></div>
         <div></div>
         <div></div>
