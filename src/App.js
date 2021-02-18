14f43a9c4a0efc1dad38c95bf330909a5bb53cf6
Ben8842 <z87e99b44r55a@gmail.com>
2021-02-17 23:26:10 -0500

    more change

--- src/App.js
+++ src/App.js
@@ -5,30 +5,40 @@ function App() {
   return (
     <div>
       <div id="title">Recent Projects</div>
-      <div class="fancy">
-        <button class="fancy" variant="primary" size="lg" block>
-          <a href="https://trivia-redux.herokuapp.com/">TRIVIA</a>
-        </button>
+      <div class="row">
+        <div class="column">
+          <div class="fancy">
+            <button class="fancy" variant="primary" size="lg" block>
+              <a href="https://trivia-redux.herokuapp.com/">TRIVIA</a>
+            </button>
 
-        <button class="fancy" variant="secondary" size="lg" block>
-          <a href="https://emojii-matching.herokuapp.com/">EMOJII MATCHING</a>
-        </button>
-        <button class="fancy" variant="primary" size="lg" block>
-          <a href="https://wordsearch-generator.herokuapp.com/">
-            WORD SEARCH GENERATOR
-          </a>
-        </button>
-        <button class="fancy" variant="secondary" size="lg" block>
-          <a href="https://benwperkins.github.io/BitProject/">
-            Cryptocurrency Project
-          </a>
-        </button>
-        <div class="fancy">
-          <p>Name: Ben</p>
-          <p>Contact email: ben@protonmail.com</p>
-          <p>Location: Massachusetts</p>
-          <p>About: Ben is searching for employment. </p>
+            <button class="fancy" variant="secondary" size="lg" block>
+              <a href="https://emojii-matching.herokuapp.com/">
+                EMOJII MATCHING
+              </a>
+            </button>
+            <button class="fancy" variant="primary" size="lg" block>
+              <a href="https://wordsearch-generator.herokuapp.com/">
+                WORD SEARCH GENERATOR
+              </a>
+            </button>
+            <button class="fancy" variant="secondary" size="lg" block>
+              <a href="https://benwperkins.github.io/BitProject/">
+                Cryptocurrency Project
+              </a>
+            </button>
+          </div>
+          <div class="column">
+            {" "}
+            <div>
+              <p>Name: Ben</p>
+              <p>Contact email: ben@protonmail.com</p>
+              <p>Location: Massachusetts</p>
+              <p>About: Ben is searching for employment. </p>
+            </div>
+          </div>
         </div>
+
         <div></div>
         <div></div>
       </div>
